49c6ad8fbb6d5574dd02cd6b59d59c900c429e1c
Lucas Rau <lucasmrau@gmail.com>
2023-06-16 13:20:17 -0400

    commit3

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,12 +1,13 @@
 import Fastify from 'fastify';
 import cors from '@fastify/cors'
-import { prisma } from './lib/prisma'
+import { appRoutes } from './lib/routes';
 
 
 const app = Fastify()
 
 
 app.register(cors)
+app.register(appRoutes)
 
 app.listen({
     port: 3333,
